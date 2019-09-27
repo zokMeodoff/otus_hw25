605f7dc0d7b05fb382c40107b16adea07eabbda9
zokMeodoff <47520450+zokMeodoff@users.noreply.github.com>
2019-09-27 12:09:19 +0200

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,11 +8,11 @@ class Header extends Component {
         return (
             <header className="header page__header">
                 <div className="container header__container">
-                    <Link to="/courses" className="header__logo">OTUS</Link>
-					<Link to="/courses" className="header__menu_item" id="courses_menu_item">Курсы</Link>
-                    <Link to="/login" className="header__menu_item_right">Вход</Link>
-					<Link to="/register" className="header__menu_item">Регистрация</Link>
-                    <Link to="/account" className="header__menu_item">Мой профиль</Link>
+			<Link to="/courses" className="header__logo">OTUS</Link>
+			<Link to="/courses" className="header__menu_item" id="courses_menu_item">Курсы</Link>
+			<Link to="/login" className="header__menu_item_right">Вход</Link>
+			<Link to="/register" className="header__menu_item">Регистрация</Link>
+			<Link to="/account" className="header__menu_item">Мой профиль</Link>
                 </div>
             </header>
         );
