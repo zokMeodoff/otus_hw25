47f2188b45f0e41406559f25f1e999ba70685e76
zokMeodoff <47520450+zokMeodoff@users.noreply.github.com>
2019-09-27 12:17:43 +0200

    Update AccountForm.js

--- src/components/user/AccountForm.js
+++ src/components/user/AccountForm.js
@@ -11,58 +11,58 @@ axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN';
 
 class AccountForm extends Component {
 	constructor(props) {
-        super(props);
+		super(props);
 
-        this.state = {
-            username: '',
-            firstName: '',
-            lastName: '',
-            email: '',
+		this.state = {
+			username: '',
+			firstName: '',
+			lastName: '',
+			email: '',
 			birthday: '',
 			loaded: false,
 			errors: []
-        }
+		}
 
-        this.handleUsernameChanged = this.handleUsernameChanged.bind(this);
-        this.handleFirstNameChanged = this.handleFirstNameChanged.bind(this);
-        this.handleLastNameChanged = this.handleLastNameChanged.bind(this);
-        this.handleEmailChanged = this.handleEmailChanged.bind(this);
-        this.handleBirthdayChanged = this.handleBirthdayChanged.bind(this);
-        this.handleUpdateUserData = this.handleUpdateUserData.bind(this);
-    }
+		this.handleUsernameChanged = this.handleUsernameChanged.bind(this);
+		this.handleFirstNameChanged = this.handleFirstNameChanged.bind(this);
+		this.handleLastNameChanged = this.handleLastNameChanged.bind(this);
+		this.handleEmailChanged = this.handleEmailChanged.bind(this);
+		this.handleBirthdayChanged = this.handleBirthdayChanged.bind(this);
+		this.handleUpdateUserData = this.handleUpdateUserData.bind(this);
+    	}
 
 	handleUsernameChanged = (event) => {
-        this.setState({username: event.target.value});
-    };
+		this.setState({username: event.target.value});
+	};
 
-    handleFirstNameChanged = (event) => {
-        this.setState({firstName: event.target.value});
-    };
+	handleFirstNameChanged = (event) => {
+		this.setState({firstName: event.target.value});
+	};
 
-    handleLastNameChanged = (event) => {
-        this.setState({lastName: event.target.value});
-    };
+	handleLastNameChanged = (event) => {
+		this.setState({lastName: event.target.value});
+	};
 
-    handleEmailChanged = (event) => {
-        this.setState({email: event.target.value});
-    };
+	handleEmailChanged = (event) => {
+		this.setState({email: event.target.value});
+	};
 
-    handleBirthdayChanged = (event) => {
-        this.setState({birthday: format(event.target.value, 'yyyy-MM-dd')});
-    };
+	handleBirthdayChanged = (event) => {
+		this.setState({birthday: format(event.target.value, 'yyyy-MM-dd')});
+	};
 
-    handleUpdateUserData = (event) => {
-    	event.preventDefault();
-        axios.post('/api/account/', {
-        	"username": this.state.username,
-            "firstName": this.state.firstName,
+	handleUpdateUserData = (event) => {
+		event.preventDefault();
+		axios.post('/api/account/', {
+			"username": this.state.username,
+			"firstName": this.state.firstName,
 			"lastName": this.state.lastName,
 			"email": this.state.email,
 			"birthday": this.state.birthday
-		}).then(response => {
-					this.setState({loaded: true})
-                    alert('Данные профиля были успешно обновлены!');
-                }
+			}).then(response => {
+				this.setState({loaded: true})
+				alert('Данные профиля были успешно обновлены!');
+			}
 			).catch(errors => {
 				if (errors.response) {
 					this.setState({loaded: true, errors: errors.response.data['errors']})
@@ -70,26 +70,26 @@ class AccountForm extends Component {
 				}
 			}
 		)
-    };
+	};
 
 	async componentDidMount() {
-        try {
-            let response = await axios.get('/api/account/');
-            if (response.status === 200) {
-                this.setState({username: response.data['username'],
-						firstName: response.data['firstName'],
-						secondName: response.data['secondName'],
-						email: response.data['email'],
-                        birthday: response.data['birthday'],
-                        loaded: true
-                    }
-                )
-            }
-        } catch (errors) {
-        	this.setState({errors: errors.response.data['errors']})
-            alert(errors.response.data['errors']);
-        }
-    }
+		try {
+		    let response = await axios.get('/api/account/');
+		    if (response.status === 200) {
+			this.setState({username: response.data['username'],
+							firstName: response.data['firstName'],
+							secondName: response.data['secondName'],
+							email: response.data['email'],
+				birthday: response.data['birthday'],
+				loaded: true
+			    }
+			)
+		    }
+		} catch (errors) {
+			this.setState({errors: errors.response.data['errors']})
+		    alert(errors.response.data['errors']);
+		}
+    	}
 
 	render() {
 		if (this.state.loaded){
@@ -179,10 +179,10 @@ class AccountForm extends Component {
 		else {
 			return(
 				<div className="container">
-                    <div className="row row_justify_space-between">
-                        <h2>Загрузка данных профиля...</h2>
-                    </div>
-                </div>
+                    			<div className="row row_justify_space-between">
+                        			<h2>Загрузка данных профиля...</h2>
+                    			</div>
+                		</div>
 			)
 		}
 
