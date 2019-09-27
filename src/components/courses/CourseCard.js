2ba4993c0b96c36479b669e96cf72109825c963b
zokMeodoff <47520450+zokMeodoff@users.noreply.github.com>
2019-09-27 12:10:11 +0200

    Update CourseCard.js

--- src/components/courses/CourseCard.js
+++ src/components/courses/CourseCard.js
@@ -6,12 +6,12 @@ class CourseCard extends Component {
         const {course} = this.props;
         return (
             <div className="course-card">
-				<h1 className="course-card__header">{course.name}</h1>
-				<div className="course-card__description">
-					{course.description}
-					<p className="course-card__date">C {course.startDate} &nbsp; {course.duration} месяцев</p>
-				</div>
-			</div>
+		<h1 className="course-card__header">{course.name}</h1>
+		<div className="course-card__description">
+			{course.description}
+			<p className="course-card__date">C {course.startDate} &nbsp; {course.duration} месяцев</p>
+		</div>
+	</div>
         )
     }
 }
